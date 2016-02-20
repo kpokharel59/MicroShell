bc2bd2d5d12e36c3fd572bfe579e4642048d13b0
kpokhar <kpokhar@ncsu.edu>
2016-02-20 14:24:37 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,109 @@
+/******************************************************************************
+ *
+ *  File Name........: main.c
+ *
+ *  Description......: Simple driver program for ush's parser
+ *
+ *  Author...........: Vincent W. Freeh
+ *
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "parse.h"
+
+static void prCmd(Cmd c)
+{
+  int i;
+
+  if ( c ) {
+    printf("%s%s ", c->exec == Tamp ? "BG " : "", c->args[0]);
+    if ( c->in == Tin )
+      printf("<(%s) ", c->infile);
+    if ( c->out != Tnil )
+      switch ( c->out ) {
+      case Tout:
+	printf(">(%s) ", c->outfile);
+	break;
+      case Tapp:
+	printf(">>(%s) ", c->outfile);
+	break;
+      case ToutErr:
+	printf(">&(%s) ", c->outfile);
+	break;
+      case TappErr:
+	printf(">>&(%s) ", c->outfile);
+	break;
+      case Tpipe:
+	printf("| ");
+	break;
+      case TpipeErr:
+	printf("|& ");
+	break;
+      default:
+	fprintf(stderr, "Shouldn't get here\n");
+	exit(-1);
+      }
+
+    if ( c->nargs > 1 ) {
+      printf("[");
+      for ( i = 1; c->args[i] != NULL; i++ )
+	printf("%d:%s,", i, c->args[i]);
+      printf("\b]");
+    }
+    putchar('\n');
+    // this driver understands one command
+    if ( !strcmp(c->args[0], "end") )
+      exit(0);
+  }
+}
+
+static void prPipe(Pipe p)
+{
+  int i = 0;
+  Cmd c;
+
+  if ( p == NULL )
+    return;
+
+  printf("Begin pipe%s\n", p->type == Pout ? "" : " Error");
+  for ( c = p->head; c != NULL; c = c->next ) {
+    printf("  Cmd #%d: ", ++i);
+    prCmd(c);
+  }
+  printf("End pipe\n");
+  prPipe(p->next);
+}
+
+void loadRCFileFromHome()
+{
+  const char* homeEnvVariable="HOME";
+  // get the size of home path string otherwise throws seg fault if memory not allocated before hand
+  char *homePath=malloc(sizeof(char*)*strlen(getenv(homeEnvVariable)));
+
+  //path to ushrc file
+  strcpy(homePath,getenv(homeEnvVariable));
+
+  strcat(homePath,"/");
+  strcat(homePath,".ushrc");
+  printf("%s hello\n",homePath);
+
+}
+
+int main(int argc, char *argv[])
+{
+  Pipe p;
+  char *host = "armadillo";
+  loadRCFileFromHome();
+
+  while ( 1 ) {
+    printf("%s%% ", host);
+    p = parse();
+    prPipe(p);
+    freePipe(p);
+  }
+}
+
+/*........................ end of main.c ....................................*/
